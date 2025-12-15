02bdb16f35d040d7a112add3d87bb6a4fc9b65c1
LechugaCuantica <osoriojuancamilo316@gmail.com>
2025-12-15 11:46:05 -0500

    Update estudiantes.py

---
+++ estudiantes.py
@@ -0,0 +1,18 @@
+estudiantes = {
+    "Luis Alfosno":  23 ,
+    "Arracacho": 25
+}
+
+# Todos los estudiantes
+print(estudiantes)
+
+# Agregamos un estudiante
+estudiantes["Carlos"] = 25
+
+#Ya con el agregado
+print(estudiantes)
+
+
+print(estudiantes["Carlos"])
+
+
